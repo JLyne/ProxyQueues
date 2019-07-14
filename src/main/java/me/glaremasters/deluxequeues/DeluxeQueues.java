7c2738b80886625a81c17dba15623913bed129f9
darbyjack <admin@glaremasters.me>
2019-07-13 20:37:37 -0500

    Initial

---
+++ src/main/java/me/glaremasters/deluxequeues/DeluxeQueues.java
@@ -0,0 +1,16 @@
+package me.glaremasters.deluxequeues;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class DeluxeQueues extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
