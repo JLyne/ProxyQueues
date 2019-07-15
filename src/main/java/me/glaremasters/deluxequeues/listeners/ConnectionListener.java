47ee689864b13bea6d73a877edc87fe58c544050
darbyjack <admin@glaremasters.me>
2019-07-15 16:46:22 -0500

    Added back in donor check.

--- src/main/java/me/glaremasters/deluxequeues/listeners/ConnectionListener.java
+++ src/main/java/me/glaremasters/deluxequeues/listeners/ConnectionListener.java
@@ -11,6 +11,7 @@ import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 /**
  * Created by Glare
@@ -29,34 +30,35 @@ public class ConnectionListener implements Listener {
         this.settingsManager = deluxeQueues.getSettingsHandler().getSettingsManager();
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onJoin(ServerConnectEvent event) {
         // Get the server in the event
         ServerInfo server = event.getTarget();
         // Get the player in the event
         ProxiedPlayer player = event.getPlayer();
-        // Bypass if they are a donator
-        if (player.hasPermission(settingsManager.getProperty(ConfigOptions.DONATOR_PERMISSION))) {
-            return;
-        }
-        // Check if the server has a queue
-        if (queueHandler.checkForQueue(server)) {
-            // Get the queue
-            DeluxeQueue queue = queueHandler.getQueue(server);
-            // Make sure it doesn't contain the player
-            if (!queue.checkForPlayer(player)) {
-                // Make sure they aren't joining the proxy for the first time
-                if (event.getReason() != ServerConnectEvent.Reason.JOIN_PROXY) {
-                    // Cancel the event so they don't go right away
-                    event.setCancelled(true);
-                    // Add the player to the queue
-                    queue.addPlayer(player);
+        // Create a boolean for bypass with donator
+        boolean bypass = player.hasPermission(settingsManager.getProperty(ConfigOptions.DONATOR_PERMISSION));
+        // Run this if they dont bypass
+        if (!bypass) {
+            // Check if the server has a queue
+            if (queueHandler.checkForQueue(server)) {
+                // Get the queue
+                DeluxeQueue queue = queueHandler.getQueue(server);
+                // Make sure it doesn't contain the player
+                if (!queue.checkForPlayer(player)) {
+                    // Make sure they aren't joining the proxy for the first time
+                    if (event.getReason() != ServerConnectEvent.Reason.JOIN_PROXY) {
+                        // Cancel the event so they don't go right away
+                        event.setCancelled(true);
+                        // Add the player to the queue
+                        queue.addPlayer(player);
+                    }
                 }
             }
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onLeave(PlayerDisconnectEvent event) {
         // Remove player from all queues
         queueHandler.clearPlayer(event.getPlayer());
