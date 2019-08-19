9cb408233420a741c6aacab578807ca09d833bbf
darbyjack <admin@glaremasters.me>
2019-08-19 16:16:12 -0500

    Push version change

--- src/main/java/me/glaremasters/deluxequeues/commands/CommandLeave.java
+++ src/main/java/me/glaremasters/deluxequeues/commands/CommandLeave.java
@@ -1,6 +1,5 @@
 package me.glaremasters.deluxequeues.commands;
 
-import co.aikar.commands.ACFBungeeUtil;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
