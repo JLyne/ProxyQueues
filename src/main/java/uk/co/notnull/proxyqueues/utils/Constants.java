71906bf2f1c36a6000696f696b2d10c831b55048
James Lyne <jim+github@not-null.co.uk>
2021-02-07 14:40:06 +0000

    Fix admin permission

--- src/main/java/uk/co/notnull/proxyqueues/utils/Constants.java
+++ src/main/java/uk/co/notnull/proxyqueues/utils/Constants.java
@@ -3,6 +3,6 @@ package uk.co.notnull.proxyqueues.utils;
 public class Constants {
 
     public static final String BASE_PERM = "proxyqueues.";
-    public static final String ADMIN_PERM = BASE_PERM + ".admin";
+    public static final String ADMIN_PERM = BASE_PERM + "admin";
 
 }
