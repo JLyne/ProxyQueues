6fd9ac03390440bc060c60bd0e9595ed03316ff2
James Lyne <jim+github@not-null.co.uk>
2021-07-06 00:35:45 +0100

    License headers

--- src/main/java/uk/co/notnull/proxyqueues/configuration/sections/ConfigOptions.java
+++ src/main/java/uk/co/notnull/proxyqueues/configuration/sections/ConfigOptions.java
@@ -1,3 +1,28 @@
+/*
+ * ProxyDiscord, a Velocity Discord bot
+ * Copyright (c) 2021 James Lyne
+ *
+ * Some portions of this file were taken from https://github.com/darbyjack/DeluxeQueues
+ * These portions are Copyright (c) 2019 Glare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package uk.co.notnull.proxyqueues.configuration.sections;
 
 import ch.jalu.configme.Comment;
@@ -10,11 +35,7 @@ import java.util.List;
 import static ch.jalu.configme.properties.PropertyInitializer.newListProperty;
 import static ch.jalu.configme.properties.PropertyInitializer.newProperty;
 
-/**
- * Created by Glare
- * Date: 7/13/2019
- * Time: 10:37 PM
- */
+
 public class ConfigOptions implements SettingsHolder {
     @Comment("How many seconds should be in-between each queue movement?")
     public static final Property<Integer> DELAY_LENGTH =
--- src/main/java/uk/co/notnull/proxyqueues/queues/ProxyQueue.java
+++ src/main/java/uk/co/notnull/proxyqueues/queues/ProxyQueue.java
@@ -1,3 +1,28 @@
+/*
+ * ProxyDiscord, a Velocity Discord bot
+ * Copyright (c) 2021 James Lyne
+ *
+ * Some portions of this file were taken from https://github.com/darbyjack/DeluxeQueues
+ * These portions are Copyright (c) 2019 Glare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package uk.co.notnull.proxyqueues.queues;
 
 import ch.jalu.configme.SettingsManager;
@@ -23,11 +48,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-/**
- * Created by Glare
- * Date: 7/13/2019
- * Time: 10:30 PM
- */
+
 public class ProxyQueue {
 
     private final ProxyQueues proxyQueues;
